428bec592d341fd26e91e94a35f6607939582314
kstrize <karlo.strize@gmail.com>
2014-12-11 18:45:25 +0100

    Dodana klasa Opis, layout activity_opis i dodana klasa u manifest

---
+++ StaraJezgraGradaSplita/src/com/example/starajezgragradasplita/Opis.java
@@ -0,0 +1,35 @@
+package com.example.starajezgragradasplita;
+
+import android.app.Application;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.mbrizic.starajezgragradasplita.R;
+
+public class Opis extends ActionBarActivity{
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opis);
+        
+        Toast.makeText(this, "Opis!", Toast.LENGTH_LONG).show();
+    }
+}
